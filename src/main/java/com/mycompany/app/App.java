20192417561f482e0c63b30c5c7bd59fd066868c
uUtkuC <utkuce7@gmail.com>
2024-03-08 13:12:50 +0300

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,6 +1,17 @@
 package com.mycompany.app;
 
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 
 /**
  * Hello world!
@@ -8,11 +19,76 @@ import java.util.ArrayList;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
 
-        //System.out.println( "Hello World!" );
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+            //System.out.println(req.queryParams("input1"));
+            //System.out.println(req.queryParams("input2"));
+
+            String inputArr1 = req.queryParams("inputArr1");
+            java.util.Scanner sc1 = new java.util.Scanner(inputArr1);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                inputList.add(value);
+            }
+            System.out.println(inputList);
+
+            String inputArr2 = req.queryParams("inputArr2");
+            java.util.Scanner sc2 = new java.util.Scanner(inputArr2);
+            sc2.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
+            while (sc2.hasNext())
+            {
+                int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+                inputList2.add(value);
+            }
+            System.out.println(inputList2);
+
+            int[] inputParam1Arr = new int[inputList.size()];
+            for(int i=0; i < inputParam1Arr.length ;i++){
+                inputParam1Arr[i] = inputList.get(i);
+            }
+
+            String input2 = req.queryParams("input2").replaceAll("\\s","");
+            int input2AsInt = Integer.parseInt(input2);
+
+            String inputBool = req.queryParams("inputBool 1 or 0").replaceAll("\\s","");
+            int inputBoolAsInt = Integer.parseInt(inputBool);
+
+            boolean valid = false;
+            if( inputBoolAsInt == 1) valid = true;
+
+            boolean result = App.firstArraySumGreaterThanSecondArraySumAndAThresholdValueWhenBooleanValidIsTrue(inputParam1Arr,inputList2, input2AsInt,valid);
+
+            Map map = new HashMap();
+            map.put("result", result);
+            return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+                (rq, rs) -> {
+                    Map map = new HashMap();
+                    map.put("result", "not computed yet!");
+                    return new ModelAndView(map, "compute.mustache");
+                },
+                new MustacheTemplateEngine());
     }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
     public static boolean firstArraySumGreaterThanSecondArraySumAndAThresholdValueWhenBooleanValidIsTrue
             (int [] ar1, ArrayList<Integer> arayList, int threshold, boolean valid){
             if(valid == true) {
