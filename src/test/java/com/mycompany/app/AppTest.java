b1a0287f8b6d1ab0764037ad9d39576e08ce7bae
uUtkuC <utkuce7@gmail.com>
2024-03-09 21:51:23 +0300

    updateFromHomework

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,4 +1,5 @@
-package com.mycompany.app;
+
+/*package com.mycompany.app;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,17 +8,9 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
     private App app;
     // following are variables that are to be used given to app as parameters
     private int [] inpArr;
@@ -43,9 +36,7 @@ public class AppTest
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
+
     public static Test suite()
     {
 
@@ -56,9 +47,7 @@ public class AppTest
        assertEquals("True",1,1);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+
     public void testApp()
     {
 
@@ -66,3 +55,47 @@ public class AppTest
     }
 
 }
+*/
+public class AppTest
+        extends TestCase
+{
+    private App app;
+    // following are variables that are to be used given to app as parameters
+    private int [] inpArr;
+    private ArrayList<Integer> inpArrayList;
+    private int threshold;
+    private int valid;
+
+    //following are expected results::
+    private int [] ExpectedinpArr;
+    private ArrayList<Integer> ExpectedinpArrayList;
+    private int Expectedthreshold;
+    private int Expectedvalid;
+
+
+
+    public void initialize(){app = new App();};
+    // constructor method that accepts input and output.
+
+
+    public void testFound() {
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertTrue(new App().search(array, 4));
+    }
+
+    public void testNotFound() {
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertFalse(new App().search(array, 5));
+    }
+
+    public void testEmptyArray() {
+        ArrayList<Integer> array = new ArrayList<>();
+        assertFalse(new App().search(array, 1));
+    }
+
+    public void testNull() {
+        assertFalse(new App().search(null, 1));
+    }
+
+}
+
